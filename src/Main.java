54a5e2a05f633cf4a200382b73a6588dc33cbed0
¨JoseMiguel¨ <josemiguel.gallego.52975.ikasle.egibide.org>
2025-11-04 20:30:48 +0100

    JoseMiguel: Entrada de datos y resultados

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Diez productos");
+        int contador = 0;
+
+        String descripcionMasLarga = "";
+        int longitudMasLarga = 0;
+
+        String descripcionMasUnidades = "";
+        int maxUnidades = -1;
+
+        String descripcionMasPronto = "";
+        LocalDate fechaMasPronto = null;
+        boolean salir = false;
+
+        do {
+            String descripcion = pedirDescripcion(contador + 1);
+            if (descripcion.equals("0")) {
+                salir = true;
+                break;
+            }
+            int unidades = pedirUnidades();
+            if (unidades == 0) {
+                salir = true;
+                break;
+            }
+            LocalDate fechaCaducidad = pedirFecha();
+
+            if (descripcion.length() > longitudMasLarga) {
+                longitudMasLarga = descripcion.length();
+                descripcionMasLarga = descripcion;
+            }
+
+            if (unidades > maxUnidades) {
+                maxUnidades = unidades;
+                descripcionMasUnidades = descripcion;
+            }
+
+            if (fechaMasPronto == null || fechaCaducidad.isBefore(fechaMasPronto)) {
+                fechaMasPronto = fechaCaducidad;
+                descripcionMasPronto = descripcion;
+            }
+            contador++;
+
+        } while (contador < 3 && !salir) ;
+
+        JOptionPane.showMessageDialog(null,
+                "RESULTADOS:\n" +
+                        "Producto con descripción más larga: " + descripcionMasLarga + "\n" +
+                        "Producto con más unidades: " + descripcionMasUnidades + " (" + maxUnidades + " unidades)\n" +
+                        "Producto que caduca primero: " + descripcionMasPronto + " (Caduca: " + fechaMasPronto + ")"
+        );
+        JOptionPane.showMessageDialog(null, "Programa finalizado");
     }
 }
